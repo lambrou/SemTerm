48cbbbde186dbeebd327d5e15b87450d57e8854e
lambrou <alexanderlambrou0602@gmail.com>
2023-05-01 22:40:33 -0700

    Heavily modified the prompt templates. Agent Errors have decreased significantly.

--- semterm/agent/TerminalAgentPrompt.py
+++ semterm/agent/TerminalAgentPrompt.py
@@ -7,64 +7,48 @@ If you need to install a program to use it, use the Human tool to get permission
 Your current directory is {current_directory}
 """
 
-FORMAT_INSTRUCTIONS = """RESPONSE FORMAT INSTRUCTIONS
-----------------------------
-
-When responding to me, please output a response in one of two formats:
+FORMAT_INSTRUCTIONS = """Begin Response Format instructions. ---
+Respond to the user in one of two formats:
 
 **Option 1:**
 Use this if you want to use a tool.
-Markdown code snippet formatted in the following schema:
+JSON formatted in the following schema:
 
+Thought: Here is where you will plan out your next steps
 ```json
 {{{{
     "action": string \\ The action to take. Must be one of {tool_names}
-    "action_input": string \\ The input to the action
+    "action_input": string or list of strings \\ The input to the action
 }}}}
 ```
 
 **Option #2:**
-Use this if you want to respond directly to the human. Markdown code snippet formatted in the following schema:
-
+Use this if you want to respond directly to the human. JSON formatted in the following schema:
+Thought: Here is where you will plan out your next steps
 ```json
 {{{{
     "action": "Final Answer",
-    "action_input": string \\ You should put what you want to return to use here
+    "action_input": string \\ Use this to give a final answer or ask a question.
 }}}}
 ```
-
-Do not give me any other output. I will not be able to parse it. For example, the following is WRONG:
--- EXAMPLE START --
-```json
-{{{{
-    "action": string \\ The action to take. Must be one of {tool_names}
-    "action_input": string \\ The input to the action
-}}}}
-```
-<Explanation of action taken> - This is the part this is WRONG. Do not include any explanation of the action, only the action.
--- EXAMPLE END --
 """
 
-SUFFIX = """TOOLS
-------
-You can use tools to complete tasks the user asks you to do. The tools you can use are:
+SUFFIX = """You can use tools to complete tasks the user asks you to do. The tools you can use are:
 
 {{tools}}
 
 {format_instructions}
 
-USER'S INPUT -------------------- Here is the user's input (remember to respond with a markdown code snippet of a 
-json blob with a single action, and NOTHING else): 
+USER'S INPUT ---------
 
-{{{{input}}}}"""
+{{{{input}}}}
 
-TEMPLATE_TOOL_RESPONSE = """TOOL RESPONSE: 
----------------------
+Plan out what you will do to complete the task and then respond with an action.
+"""
+
+TEMPLATE_TOOL_RESPONSE = """Observation: 
+
 {observation}
 
-(THE USER CANNOT SEE THIS, IF YOU NEED TO REFERENCE IT, DISPLAY IT TO THEM)
-If you get stuck, it is bad practice to just continuously run the same command over again. 
-DO NOT JUST RUN THE SAME COMMAND OVER AND OVER AGAIN.
-Just let the user know what you see. They will be able to help you.
-Action JSON: 
+Using the information above, plan out what you will do next to complete the task and then complete it.
 """
