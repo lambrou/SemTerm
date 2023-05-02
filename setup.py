8b8b26460a2399a7009a23e0cc9e3e4967cca6c9
lambrou <alexanderlambrou0602@gmail.com>
2023-05-01 17:54:10 -0700

    updated setup.py with new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="semterm",
-    version="0.1.0",
+    version="0.1.1",
     description="The Semantic Terminal",
     long_description="The Semantic Terminal",
     author="Lambrou",
