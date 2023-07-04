c9288c84044d4e92dbf77549f002fc96e0f954e2
Alex Lambrou <alexander.lambrou@ovationcxm.com>
2023-07-04 15:04:26 -0700

    DEV-6137 - Update cloudbuild.yml to pull in openai key

--- settings/Settings.py
+++ settings/Settings.py
@@ -1,9 +1,10 @@
 from pydantic import BaseSettings
+import os
 
 
 class Settings(BaseSettings):
-    database_url: str
-    openai_api_key: str
+    database_url: str = os.environ.get("DATABASE_URL", "mongodb://localhost:27018")
+    openai_api_key: str = os.environ.get("OPENAI_API_KEY", '')
     default_llm: str = "gpt-3.5-turbo"
 
     class Config:
