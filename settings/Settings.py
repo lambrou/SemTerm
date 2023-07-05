85bfaf44e67b515b8de76bc3833ad04bdf846be8
Alex Lambrou <alexander.lambrou@ovationcxm.com>
2023-07-05 16:13:13 -0700

    DEV-6137 - Adjusted database setup. Implemented DB connection pooling.

--- settings/Settings.py
+++ settings/Settings.py
@@ -3,7 +3,7 @@ import os
 
 
 class Settings(BaseSettings):
-    database_url: str = os.environ.get("DATABASE_URL", "mongodb://localhost:27018")
+    database_url: str = os.environ.get("DATABASE_URL", "mongodb://mongodb:27017")
     openai_api_key: str = os.environ.get("OPENAI_API_KEY", '')
     default_llm: str = "gpt-3.5-turbo"
 
